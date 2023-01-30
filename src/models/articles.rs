5644afeff8471d3797db34776ba47c07051ce880
tenra <tetenchi@yahoo.co.jp>
2023-01-30 13:12:22 +0900

    first commit

---
+++ src/models/articles.rs
@@ -0,0 +1,53 @@
+use {
+    crate::schema::*,
+    serde::Serialize,
+};
+
+#[derive(Identifiable, Debug, Serialize, Queryable, Clone)]
+pub struct Article {
+    pub id: i32,
+    pub title: String,
+    pub pub_date: chrono::NaiveDateTime,
+    pub published: bool,
+    pub headline: String,
+    pub image: String,
+    pub content: String,
+}
+
+impl FromModel<Article> for ArticleRepresentation {
+    fn from_model(article: Article) -> ArticleRepresentation {
+        ArticleRepresentation {
+            id: article.id,
+            title: article.title,
+            pub_date: article.pub_date,
+            published: article.published,
+            headline: article.headline,
+            image: API_URL.to_owned() + &article.image,
+            content: article.content,
+        }
+    }
+}
+
+impl Article {
+    pub fn get(
+        id: &i32,
+        connection: &PgConnection,
+    ) -> Result<ArticleRepresentation, diesel::result::Error> {
+        let article = articles::table.find(id).first::<Article>(connection)?;
+        Ok(ArticleRepresentation::from_model(article))
+    }
+
+    pub fn list(
+        connection: &PgConnection,
+    ) -> Result<Vec<ArticleRepresentation>, diesel::result::Error> {
+        let articles = articles::table
+            .load::<Article>(connection)
+            .expect("Could not load articles.");
+        let results = articles
+            .into_iter()
+            .map(ArticleRepresentation::from_model)
+            .collect();
+
+        Ok(results)
+    }
+}
---
+++ src/schema.rs
@@ -0,0 +1,13 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    articles (id) {
+        id -> Int4,
+        title -> Text,
+        pub_date -> Timestamp,
+        published -> Bool,
+        headline -> Text,
+        image -> Text,
+        content -> Text,
+    }
+}
